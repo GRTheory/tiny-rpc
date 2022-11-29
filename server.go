f85e4a542265c055c8799466594f24df906c4321
zhangsan33 <8546260+zhangsan3356@user.noreply.gitee.com>
2022-11-29 15:32:16 +0800

    add outer server

---
+++ server.go
@@ -0,0 +1,45 @@
+package tinyrpc
+
+import (
+	"log"
+	"net"
+	"net/rpc"
+
+	"github.com/GRTheory/tiny-rpc/codec"
+	"github.com/GRTheory/tiny-rpc/serializer"
+)
+
+type Server struct {
+	*rpc.Server
+	serializer.Serializer
+}
+
+func NewServer(opts ...Option) *Server {
+	options := options{
+		serializer: serializer.Proto,
+	}
+	for _, option := range opts {
+		option(&options)
+	}
+
+	return &Server{&rpc.Server{}, options.serializer}
+}
+
+func (s *Server) Register(rcvr interface{}) error{
+	return s.Server.Register(rcvr)
+}
+
+func (s *Server) RegisterName(name string, rcvr interface{}) error{
+	return s.Server.RegisterName(name, rcvr)
+}
+
+func (s *Server) Serve(lis net.Listener){
+	log.Printf("tinyrpc started on: %s", lis.Addr().String())
+	for {
+		conn, err := lis.Accept()
+		if err != nil {
+			continue
+		}
+		go s.Server.ServeCodec(codec.NewServerCodec(conn, s.Serializer))
+	}
+}
